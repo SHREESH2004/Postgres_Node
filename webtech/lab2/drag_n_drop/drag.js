b63503ea81c0ffd3762309e4225d2e1568d130ae
Shreesh Sanyal <shreeshsanyal@gmail.com>
2026-01-20 11:11:51 +0530

    Refactor webtech project structure: - Removed outdated HTML files for calculator, drag and drop, and pattern examples. - Added new calculator implementation with separate CSS and JS files. - Introduced new drag and drop functionality with dedicated styles and scripts. - Created a portfolio page with structured sections for experience, projects, skills, education, and certifications. - Ensured all new files are organized under appropriate directories for better maintainability.

---
+++ webtech/lab2/drag_n_drop/drag.js
@@ -0,0 +1,15 @@
+
+  function allowDrop(event) {
+    event.preventDefault();
+  }
+
+  function drag(event) {
+    event.dataTransfer.setData("text", event.target.id);
+  }
+
+  function drop(event) {
+    event.preventDefault();
+    const data = event.dataTransfer.getData("text");
+    event.target.innerHTML = "";
+    event.target.appendChild(document.getElementById(data));
+  }
